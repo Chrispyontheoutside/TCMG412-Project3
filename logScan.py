f39a1e2266861b537ef32e75f90519a84891667c
Chris <chrispeterfrancis@tamu.edu>
2021-02-22 19:03:29 -0600

    Fixed decoding issues

--- logScan.py
+++ logScan.py
@@ -1,15 +1,14 @@
-import urllib.request
+from urllib.request import urlopen
 import os.path
-from os import paths
+from os import path
 def main():
     getRequests = 0
     getRequestsPrevYear = 0
     if(not path.exists('local/log.txt')):
-        resourceUrl = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-        data = resourceUrl.read()
+        resourceUrl = urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
         f = open('local/log.txt','w')
-        f.write(data)
-        f.close
+        f.write(resourceUrl.read().decode('utf-8'))
+        f.close()
     f = open('local/log.txt', 'r')
     for line in f:
         if "GET" in line:
